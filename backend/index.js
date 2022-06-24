0af387654660bd4f5c243a56a6baa8435222ed4e
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-06-25 01:56:44 +0700

    tinggal backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+const authRoutes = require("./routes/auth");
 
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
